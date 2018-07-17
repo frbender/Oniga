7a541b946a31319092a985eb7088db14ace79ce6
frbender <franz.bender@icloud.com>
2018-07-17 02:03:43 +0200

    More fun

--- main.py
+++ main.py
@@ -34,14 +34,15 @@ def get_accessory(driver):
 # Start the accessory on port 51826
 driver = AccessoryDriver(port=51826)
 
+a = get_accessory(driver)
 # Change `get_accessory` to `get_bridge` if you want to run a Bridge.
-driver.add_accessory(accessory=get_accessory(driver))
+driver.add_accessory(accessory=a)
 
 # We want SIGTERM (kill) to be handled by the driver itself,
 # so that it can gracefully stop the accessory, server and advertising.
 signal.signal(signal.SIGTERM, driver.signal_handler)
 
-driver.setup_message()
+a.setup_message()
 
 # Start it!
 driver.start()
